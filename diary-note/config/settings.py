ef73a4332bce1424005f65a224683225784ee3c9
Donkey-1028 <anbo10283182@gmail.com>
2020-02-06 17:36:18 +0900

    restframework 추가

--- diary-note/config/settings.py
+++ diary-note/config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'diary'
+    'diary',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
