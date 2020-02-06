4bbcc9dd36c14c608356398e091172e1e27e2048
Donkey-1028 <anbo10283182@gmail.com>
2020-02-06 14:20:51 +0900

    admin 추가

--- diary-note/diary/admin.py
+++ diary-note/diary/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Diary
 
+
+class DiaryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'date', 'title', 'created', 'updated']
+
+
+admin.site.register(Diary, DiaryAdmin)
 # Register your models here.
