5d62b2936bb090b8eac1c60746976101d96c2095
Donkey-1028 <anbo10283182@gmail.com>
2020-02-12 13:40:00 +0900

    로그아웃 추가

--- diary-note/users/urls.py
+++ diary-note/users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import UserRegistrationAPIView, UserLoginAPIView, UserTokenAPIView
+from .views import UserRegistrationAPIView, UserLoginAPIView, UserTokenAPIView, UserLogoutAPIView
 
 app_name = 'users'
 
 urlpatterns = [
     path('registration/', UserRegistrationAPIView.as_view(), name='registration'),
     path('login/', UserLoginAPIView.as_view(), name='login'),
+    path('logout/', UserLogoutAPIView.as_view(), name='logout'),
     path('token/<key>', UserTokenAPIView.as_view(), name='token'),
 ]
--- diary-note/users/views.py
+++ diary-note/users/views.py
@@ -3,7 +3,9 @@ from django.db.models.signals import post_save
 from django.conf import settings
 
 from rest_framework import status
+from rest_framework.views import APIView
 from rest_framework.generics import CreateAPIView, GenericAPIView, RetrieveDestroyAPIView
+from rest_framework.authentication import SessionAuthentication
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 
@@ -24,10 +26,10 @@ class UserRegistrationAPIView(CreateAPIView):
         # Called by CreateModelMixin when saving a new object instance.
         self.perform_create(serializer)
 
-        user = serializer.instance
-        token, created = Token.objects.get_or_create(user=user)
         data = serializer.data
-        data["token"] = token.key
+        """user = serializer.instance
+        token, created = Token.objects.get_or_create(user=user)
+        data["token"] = token.key"""
 
         headers = self.get_success_headers(serializer.data)
         return Response(data, status=status.HTTP_201_CREATED, headers=headers)
@@ -60,24 +62,18 @@ class UserLoginAPIView(GenericAPIView):
             )
 
 
+class UserLogoutAPIView(APIView):
+    """로그아웃"""
+    def get(self, request):
+        request.session.delete()
+        return Response(status=status.HTTP_200_OK)
+
+
 class UserTokenAPIView(RetrieveDestroyAPIView):
+    """READ , DESTROY"""
     lookup_field = "key"
     serializer_class = TokenSerializer
     queryset = Token.objects.all()
 
-    def filter_queryset(self, queryset):
-        # 쿼리셋을 주면 사용되는 백엔드에서 새로운 쿼리로 리턴하는 메서드
-        return queryset.filter(user=self.request.user)
 
-    def retrieve(self, request, key, *args, **kwargs):
-        if key == "current":
-            instance = Token.objects.get(key=request.auth.key)
-            serializer = self.get_serializer(instance)
-            return Response(serializer.data)
-        return super(UserTokenAPIView, self).retrieve(request, key, *args, **kwargs)
 
-    def destroy(self, request, key, *args, **kwargs):
-        if key == "current":
-            Token.objects.get(key=request.auth.key).delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-        return super(UserTokenAPIView, self).destroy(request, key, *args, **kwargs)
