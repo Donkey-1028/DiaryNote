27cda3b227a1c54ec123592448ea2ed098138543
Donkey-1028 <anbo10283182@gmail.com>
2020-02-13 14:27:22 +0900

    token test 추가

--- diary-note/users/tests.py
+++ diary-note/users/tests.py
@@ -1,8 +1,7 @@
-import json
-
 from django.urls import reverse
 
 from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
 
 # Create your tests here.
 
@@ -10,14 +9,25 @@ from rest_framework.test import APITestCase
 class UserRegistrationTest(APITestCase):
     url = reverse('users:registration')
 
-    def test_can_registration(self):
-        """유저가 제대로 생성되는지 테스트"""
-        user_data = {
+    def setUp(self):
+        self.user_data = {
             "username": "testuesr",
             "email": "test@testuser.com",
             "password": "test1234",
             "confirm_password": "test1234",
         }
-        response = self.client.post(self.url, user_data)
+
+    def test_can_registration(self):
+        """유저가 제대로 생성되는지 테스트"""
+        response = self.client.post(self.url, self.user_data)
+        print(response.data.get('id'))
         self.assertEqual(response.status_code, 201)
-        self.assertTrue('token' in json.loads(response.content))
+
+    def test_make_token_automatically(self):
+        """유저가 생성되었을 때 토큰이 자동으로 생성되는지"""
+        response = self.client.post(self.url, self.user_data)
+        user_id = response.data.get('id')
+        token = Token.objects.get(user_id=user_id)
+        self.assertEqual(token.user.id, user_id)
+
+
