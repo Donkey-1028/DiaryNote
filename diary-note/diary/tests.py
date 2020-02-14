ec3ad720c93d53fb39d295fffdc6a079fe5649e1
Donkey-1028 <anbo10283182@gmail.com>
2020-02-15 00:07:09 +0900

    DiaryListAPIView 테스트 케이스 추가

--- diary-note/diary/tests.py
+++ diary-note/diary/tests.py
@@ -1,39 +1,43 @@
 import datetime
-import json
 
 from django.urls import reverse
 from django.contrib.auth.models import User
 
-from rest_framework.test import APITestCase, APIRequestFactory
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
 
 from .models import Diary
-
 
 
 class DiaryListTest(APITestCase):
     url = reverse('diary:diary_list')
 
     def setUp(self):
-        """
-        테스트 기본 설정
-        """
-        self.request_factory = APIRequestFactory()
-        self.user = User.objects.create()
-        Diary.objects.create(author_id=self.user.id, date=datetime.datetime.now())
+        self.username = "testuser"
+        self.password = "test1234"
+        self.email = "test@test.com"
+        self.user = User.objects.create_user(username=self.username, password=self.password,
+                                             email=self.email)
+        self.diary = Diary.objects.create(author=self.user, date=datetime.datetime.now())
+        self.token = Token.objects.get(user=self.user)
 
-    def test_diary_list_returns_correct_data(self):
-        """
-        해당 유저의 다이어리가 리턴되는지 테스트
-        """
-        request = self.request_factory.get(self.url)
-        request.user = self.user
-        #response = diary_list(request)
-        # APIRequestFactory()를 사용하여 직접 뷰를 테스트 하는 경우 내부의 요청-응답 사이클에 의해
-        # 반한되는 response 는 rendering 된 데이터가 아니기 때문에 render 를 해줘야 한다고 함.
-        response.render()  # html/text 가 application/json 으로 렌더링
-        diary_decoded = response.content.decode()  # 가져온 바이트 코드 디코딩
-        diary_json_loaded = json.loads(diary_decoded)  # json 디코딩
-        print(diary_json_loaded)
-        diary_owner = diary_json_loaded['diaries'][0]['author']
+    def api_authentication(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
 
-        self.assertEqual(diary_owner, self.user.id)
+    def test_anonymous_user_try_see_list(self):
+        """익명 유저가 diary list 에 접근할 때 테스트"""
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 401)
+
+    def test_authenticated_user_try_see_list(self):
+        """로그인 한 유저가 diary list 에 접근할 때 테스트"""
+        self.client.login(username=self.username, password=self.password)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 401)
+
+    def test_authenticated_user_try_see_list_with_token(self):
+        """로그인 한 유저가 token 을 가지고 diary list 에 접근할 때 테스트"""
+        self.client.login(username=self.username, password=self.password)
+        self.api_authentication()
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
